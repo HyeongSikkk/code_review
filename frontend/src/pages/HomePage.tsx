02f2cd1e04d9e243b9ab995413ab586d6aadea6b
HyeongSikkk <2674625@naver.com>
2025-02-11 12:42:51 +0900

    back + front

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/homepage.css';
+
+const Homepage: React.FC = () => {
+  return (
+    <div>여긴 뭐지?</div>
+  );
+};
+
+export default Homepage;
+
+// import React from 'react';
+// import '../styles/homepage.css';
+
+// const HomePage: React.FC = () => {
+//   return (
+
+//           <div>
+//             hey
+//           </div>
+
+//   );
+// };
+
+// export default HomePage;
--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "/api/";
-
-export const getHelloMessage = async () => {
-    const response = await axios.get(`${API_URL}hello/`);
-    return response.data;
-}
